f5823514154bc66ef05672e89b1eed5bf7d3fcd7
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-07-19 03:54:46 +0800

    feat : add server running in localhost

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 5000;
 
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
