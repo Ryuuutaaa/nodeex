ead7caf221286a998a11c5f4d0d3dbf894c45eee
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-07-29 15:41:39 +0800

    feat : add post data with api

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,30 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    "firstname" : "budi",
+    "lastname" : "santoso",
+    "age" : 23
+}
+]
+
 // all routes staring the route /users
 router.get("/", (req, res) => {
-    res.send("hello")
+
+    // console.log(users);
+
+    res.send(users)
+})
+
+router.post("/", (req, res) => {
+    // console.log("POST ROUTED REACHED")
+
+    const user =  req.body;
+    users.push(user);
+
+    // res.send(`User with the name ${user.firstname} added to databse`)
+    res.send(`data masuk dengan username ${user.firstname}`)
 })
 
 export default router;
