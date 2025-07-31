ceceb1059f83bcd073108765b402420ced997ed2
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-08-01 02:14:16 +0800

    feat :  add elete user by id

--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,47 @@
-import express from "express"; 
+import express from "express";
 import { v4 as uuidv4 } from "uuid";
 uuidv4();
 
 const router = express.Router();
 
-const users = []; 
+let users = [];
 
 // all routes staring the route /users
 router.get("/", (req, res) => {
+  // console.log(users);
 
-    // console.log(users);
-
-    res.send(users)
-})
+  res.send(users);
+});
 
 router.post("/", (req, res) => {
-    // console.log("POST ROUTED REACHED")
+  // console.log("POST ROUTED REACHED")
 
-    const user =  req.body;
-    const userId = uuidv4();
+  const user = req.body;
+  const userId = uuidv4();
 
-    const withUserId = {...user, id: userId}
+  const withUserId = { ...user, id: userId };
 
-    users.push(withUserId);
+  users.push(withUserId);
 
-    // res.send(`User with the name ${user.firstname} added to databse`)
-    res.send(`data masuk dengan username ${user.firstname}`)
-})
-
+  // res.send(`User with the name ${user.firstname} added to databse`)
+  res.send(`data masuk dengan username ${user.firstname}`);
+});
 
 // /users/2 => req.params {id : 2}
 router.get("/:id", (req, res) => {
-    const {id} = req.params; 
+  const { id } = req.params;
 
-    const findUser = users.find((user) => user.id === id)
+  const findUser = users.find((user) => user.id === id);
 
-    res.send(findUser);
-})
+  res.send(findUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} deleted`);
+});
 
 export default router;
