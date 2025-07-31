ce5d956e991d13a4d39537e0f60b8b2538d02c74
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-08-01 02:29:43 +0800

    feat : add cotnroller createUser

---
+++ controllers/user-controller.js
@@ -0,0 +1,10 @@
+export const createUser = (req, res) => {
+    const user = req.body;
+    const userId = uuidv4();
+
+  const withUserId = { ...user, id: userId };
+
+  users.push(withUserId);
+
+  res.send(`data masuk dengan username ${user.firstname}`);
+}
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,18 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
+import { createUser } from "../controllers/user-controller";
 uuidv4();
 
 const router = express.Router();
 
 let users = [];
 
-// all routes staring the route /users
 router.get("/", (req, res) => {
-  // console.log(users);
 
   res.send(users);
 });
 
-router.post("/", (req, res) => {
-  // console.log("POST ROUTED REACHED")
-
-  const user = req.body;
-  const userId = uuidv4();
-
-  const withUserId = { ...user, id: userId };
-
-  users.push(withUserId);
-
-  // res.send(`User with the name ${user.firstname} added to databse`)
-  res.send(`data masuk dengan username ${user.firstname}`);
-});
-
-// /users/2 => req.params {id : 2}
+router.post("/", createUser)
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
